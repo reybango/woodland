54bcf5424901787e13a9bd225175c20c9791919f
Rey Bango <reybango@gmail.com>
2016-03-15 15:11:48 -0700

    Added nested route and embedded template

--- app/router.js
+++ app/router.js
@@ -6,8 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('orders', { path: '/orders' });
-  this.route('order', { path: '/orders/:order_id' });
+  this.route('orders', function() {
+    this.route('order', { path: '/:order_id' });
+  });
 });
 
 export default Router;
